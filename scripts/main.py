f272a06381957cc9349ac63b50b762f7f0df83c5
Marie-Eve Picard (she/her) <77584086+me-pic@users.noreply.github.com>
2022-06-12 13:29:08 -0700

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# ./main.py --path_0 "data_FEPS_0.json" --path_1 "data_FEPS_1.json" --seed 42 --model "whole-brain" --reg "lasso"
-
 import json
 import pickle
 import prepping_data
@@ -15,54 +13,123 @@ from sklearn.preprocessing import StandardScaler
 from argparse import ArgumentParser
 
 def main():
+    """
+    path
+    ----
+    Path to json file containing the data (independent variable, dependent variable, group variable if needed)
+
+    seed
+    ----
+    Value to use to set the seed
+
+    model parameter
+    ---------------
+    Areas of the brain that will be used to extract the data:
+
+    'whole-brain': extract the activity from the whole-brain
+    PATH_TO_MASK: model could take the path to a specified mask (niimg-like object)
+
+    reg parameter
+    -------------
+    Algorithm to use on the data
+
+    'lasso': Apply Lasso regression 
+    'ridge': Apply Rigde regressions
+    'svr': Apply a Support Vector Regression 
+    'svc': Apply a Support Vector Classifier
+    'lda': Apply a Linear Discriminant Analysis classifier
+    'rf': Apply a Random Forest classifier
+    'huber': Apply a Robust Huber Regression
+    'linear': Apply a linear regression 
+
+    analysis parameter
+    ------------------
+    Specify which kind of analysis to run on the data between 3 choices:
+
+    'regression': regression analysis
+    'classification': classification analysis
+    'sl': searchlight analysis
+
+    folder
+    ------
+    Where to save the data
+    """
+    
     parser = ArgumentParser()
-    parser.add_argument("--path_0", type=str)
-    parser.add_argument("--path_1", type=str)
+    parser.add_argument("--path", type=str)
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--model", type=str, choices=["whole-brain","M1","without M1"], default="whole-brain")
-    parser.add_argument("--reg", type=str, choices=['lasso','ridge','svr'], default='lasso')
+    parser.add_argument("--model", type=str, default="whole-brain")
+    parser.add_argument("--reg", type=str, choices=['lasso','ridge','svr','svc','lda','rf','huber','linear'], default='lasso')
+    parser.add_argument('--folder', type=str, default=None)
+    parser.add_argument('--confound', type=str, default=None)
+    parser.add_argument('--analysis', type=str, choices=['regression','classification','sl'], default='regression')
     args = parser.parse_args()
 
-    #Loading the datasets
-    data_0 = open(args.path_0, "r")
-    data_feps_0 = json.loads(data_0.read())
+    if args.folder == None:
+        args.folder = datetime.now().strftime("%d_%m_%Y")
     
-    data_1 = open(args.path_1, "r")
-    data_feps_1 = json.loads(data_1.read())
+    try: 
+        os.makedirs(args.folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    
+    #Loading the datasets
+    data = json.loads(open(args.path, "r").read())
 
     #Predicted variable
-    y_0 = np.array(data_feps_0["target"])
-    y_1 = np.array(data_feps_1["target"])
+    y = np.array(data["target"])
     
     #Group variable: how the data is grouped (by subjects)
-    gr_0 = data_feps_0["group"]
-    gr_1 = data_feps_1["group"]
+    gr = np.array(data["group"])
 
     #Convert fmri files to Nifti-like objects
-    array_feps = prepping_data.hdr_to_Nifti(data_feps_0["data"]+data_feps_1["data"])
+    array_feps = prepping_data.hdr_to_Nifti(data["data"])
+    
+    #Load confound file
+    if args.confound == None:
+        confound = None
+    else:
+        confound = pd.read_csv(args.confound)
+
     #Extract signal from gray matter
     if args.model == "whole-brain":
-        masker, extract_X = prepping_data.extract_signal(array_feps, mask="template", standardize = True)
-    elif args.model == "M1":
-        mask_M1 = nib.load("mask_BA4.nii")
-        extract_X = prepping_data.extract_signal_from_mask(array_feps, mask_M1)
-    elif args.model == "without M1":
-        mask_NoM1 = nib.load("mask_excluding_BA4.nii")
-        extract_X = prepping_data.extract_signal_from_mask(array_feps, mask_NoM1)
+        masker, extract_X = prepping_data.extract_signal(array_feps, mask="template", standardize = True, confound=confound)
+    else:
+        masker = nib.load(args.model)
+        extract_X = prepping_data.extract_signal_from_mask(array_feps, masker, affine=False)
     
     #Standardize the signal
     stand_X = StandardScaler().fit_transform(extract_X.T)
     X = stand_X.T
     
-    #Compute the model
+    #Define the algorithm
     if args.reg == "lasso":
         reg = Lasso()
     elif args.reg == "ridge":
         reg = Ridge()
     elif args.reg == "svr":
         reg = SVR(kernel="linear")
+    elif args.reg == "svc":
+        reg = SVC(kernel="linear", class_weight='balanced')
+    elif args.reg == "lda":
+        reg = LinearDiscriminantAnalysis()
+    elif args.reg == "rf":
+        reg = RandomForestClassifier(n_estimators=4, random_state=42)
+    elif args.reg == 'huber':
+        reg = HuberRegressor(alpha=1.0)
+    elif args.reg == 'linear':
+        reg = LinearRegression()
     
-    X_train, y_train, X_test, y_test, y_pred, model, model_voxel = building_model.train_test_model(X[:len(y_0)], y_0, gr_0,reg=reg)
+    #ANALYSIS
+    if args.analysis=="regression":
+        X_train, y_train, X_test, y_test, y_pred, model, model_voxel, df_metrics = building_model.train_test_model(X[:len(y)], y, gr, test_size=0.3,reg=reg,random_seed=42)
+    if args.analysis=="classification":
+        X_train, y_train, X_test, y_test, y_pred, model, df_metrics = building_model.train_test_classify(X, y, gr, clf=reg)
+    if args.analysis=="sl":
+        sl_scores = building_model.searchlight_analysis(X,y_0,reg=reg,mask_sl=mask_sl,mask_signal=masker.mask_img_,gr=gr,splits=5,test_size=0.3)
+        sl_scores.to_filename(os.path.join(args.folder,f"_sl_scores.nii"))
+        sys.exit()
 
     if args.model == "whole-brain" :
         for i, element in enumerate(model_voxel):
