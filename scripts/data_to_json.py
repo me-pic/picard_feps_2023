8b384b53715efca895213397fccfcfa0d1a23329
Marie-Eve Picard (she/her) <77584086+me-pic@users.noreply.github.com>
2022-12-16 16:30:19 -0500

    update data_to_json script

--- scripts/data_to_json.py
+++ scripts/data_to_json.py
@@ -1,87 +1,83 @@
 import json
 import os
+import pandas as pd
+from argparse import ArgumentParser
 
-def read_files(filesInput = None):
+def read_files(path_data):
     """
     Load all functional files into Nifti format
     
     Parameters
     ----------
-    filesInput: Folder where the functional images can be found
+    path_data: string
+        path containing the functional images
 
     Returns
     ----------
-    files_hdr: list containing the path of each functional images
-    array : ndarray containing the Nifti objects
+    files_hdr: list 
+        list containing the path of each functional images
     """
-    path, dirs, files = next(os.walk(filesInput))
+    path_files_hdr = []
 
-    files_hdr = list()
+    path, _, files = next(os.walk(path_data))
+
     for file in files:
         if file[-3:] == "hdr":
-            files_hdr.append(path + "//" + file)
+            path_files_hdr.append(path + "//" + file)
     
-    files_hdr.sort(reverse=True)
-    files_hdr.sort()
+    path_files_hdr.sort(reverse=True)
+    path_files_hdr.sort()
 
-    return files_hdr
-
-
-def check_order(dataframe, column, filenames, extension=".hdr"):
-    """
-    Check if the order of the fMRI files match the one in the
-    behavioral file.
-    
-    Parameters
-    ----------
-    dataframe (pandas dataFrame) : behavioral dataframe
-    column (String): name of the column containing the participants' ID
-    filenames (list): list of paths where the fMRI data can be found
-    extension (String): extension of the fMRI files
-    
-    Returns
-    ----------
-    order: either or not the behavioral file and the functional images are in the same order (boolean)
-    """
-    order = True
-    ls_ID = dataframe[column].tolist()
-    
-    for i, filename in enumerate(filenames):
-        if os.path.basename(filename[:-len(extension)]) != ls_ID[i]:
-            print("Error in the files order")
-            print(os.path.basename(filename[:-len(extension)]))
-            print(ls_ID[i])
-            order=False
-            break
-            
-    return order
+    return path_files_hdr
 
 
-def save_to_json(dataframe, ID, target, group = None, files, save_name):
+def save_to_json(dataframe, id, target, group, files, path_output):
     """
     Create and save a json file containing the relative path of the hdr fmri files,
     the target variable and the group variable
 
     Parameters
     ----------
-    dataframe: behavioral dataframe
-    ID (string): name of the column in dataframe containing the ID variable
-    target (string): name of the column in dataframe containing the target variable
-    group (string): name of the column in dataframe containing the group variable 
-    files: list of hdr file paths
-    save_name (string): name of the file to create
+    dataframe: dataFrame
+        dataframe containing the behavioral data
+    id: string
+        name of the column containing the participants' ID
+    target: string
+        name of the column in dataframe containing the target variable
+    group: string
+        name of the column in dataframe containing the group variable
+    files: list
+        list of hdr file paths
+    path_output: string 
+        path to save the json file
     """
-    if check_order(dataframe, ID, files):
+    filename = []
+    for file in files:
+        filename.append(os.path.splitext(file)[0].split('/')[-1])
+
+    if filename == dataframe[id].tolist():
         idx = files[0].find("//")
-        for i in range(len(files)):
-            files[i]=files[i][idx+1:]
-            files[i]=root+files[i]
+        for file in files:
+            files=files[idx+1:]
+
         if group == None:
-            FEPS_data = {"target": dataframe[target].to_list(), "group": group, "data": files}
+            data = {"target": dataframe[target].to_list(), "group": group, "data": files}
         else:
-            FEPS_data = {"target": dataframe[target].to_list(), "group": dataframe[group].to_list(), "data": files}
+            data = {"target": dataframe[target].to_list(), "group": dataframe[group].to_list(), "data": files}
         
-        with open(save_name, 'w') as fp:
-                json.dump(FEPS_data, fp)
+        with open(path_output, 'w') as fp:
+            json.dump(data, fp)
     else: 
         print("Cannot save to json: fmri and behavioral data are not in the same order")
+
+
+parser = ArgumentParser()
+parser.add_argument('--path_behavioral', type=str, default=None)
+parser.add_argument('--path_fmri', type=str, default=None)
+parser.add_argument('--path_output', type=str, default=None)
+args = parser.parse_args()
+
+df = pd.read_csv(args.path_behavioral)
+hdr_files = read_files(args.path_fmri)
+
+save_to_json(df, 'CODE', 'FACS', 'GROUP', hdr_files, args.path_output)
