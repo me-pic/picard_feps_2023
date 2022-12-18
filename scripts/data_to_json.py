19e0ab398b038ae4531219f96f966ff38472950a
Marie-Eve Picard (she/her) <77584086+me-pic@users.noreply.github.com>
2022-12-18 10:54:39 -0500

    fix bug data_to_json.py

--- scripts/data_to_json.py
+++ scripts/data_to_json.py
@@ -31,7 +31,7 @@ def read_files(path_data):
     return path_files_hdr
 
 
-def save_to_json(dataframe, id, target, group, files, path_output):
+def save_to_json(dataframe, id, target, group, files, path_output, filename_output=None):
     """
     Create and save a json file containing the relative path of the hdr fmri files,
     the target variable and the group variable
@@ -50,6 +50,8 @@ def save_to_json(dataframe, id, target, group, files, path_output):
         list of hdr file paths
     path_output: string 
         path to save the json file
+    filename_output: string
+        filename to use for the json output file
     """
     filename = []
     for file in files:
@@ -57,15 +59,18 @@ def save_to_json(dataframe, id, target, group, files, path_output):
 
     if filename == dataframe[id].tolist():
         idx = files[0].find("//")
-        for file in files:
-            files=files[idx+1:]
+        for i, file in enumerate(files):
+            files[i] = file[idx+2:]
 
         if group == None:
             data = {"target": dataframe[target].to_list(), "group": group, "data": files}
         else:
             data = {"target": dataframe[target].to_list(), "group": dataframe[group].to_list(), "data": files}
+
+        if filename_output is None:
+            filename_output = 'dataset.json'
         
-        with open(path_output, 'w') as fp:
+        with open(os.path.join(path_output, filename_output), 'w') as fp:
             json.dump(data, fp)
     else: 
         print("Cannot save to json: fmri and behavioral data are not in the same order")
@@ -75,6 +80,7 @@ parser = ArgumentParser()
 parser.add_argument('--path_behavioral', type=str, default=None)
 parser.add_argument('--path_fmri', type=str, default=None)
 parser.add_argument('--path_output', type=str, default=None)
+parser.add_argument('--filename_output', type=str, default=None)
 args = parser.parse_args()
 
 df = pd.read_csv(args.path_behavioral)
